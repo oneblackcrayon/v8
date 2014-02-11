cd9fddbbc26e4ccae8b9cb19ef8fc22da2249437
Frederick Polk <github@oneblackcrayon.com>
2014-02-11 18:05:16 -0500

    Added Jquery Custombox effects for Wufoo form.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en-us">
+<html class="no-js" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en-us">
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +7,8 @@
 		<meta name="author" content="Frederick Polk">
 		<meta name="description" content="One Black Crayon is a solo web studio that makes websites, crafts HTML emails, and helps with internet presences.">
 		<link rel="stylesheet" href="style.css">
-		<!-- add more css here -->
-		<!-- add more javascript here -->
+		<link rel="stylesheet" href="assets/jquery.custombox.css">
+		<script src="assets/modernizr.js"></script>
 		<!-- make sure your google analytics and other analytics are up to date with correct code -->
 		<script type="text/javascript">
 		(function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-XXXXX-X']); _gaq.push(['_setDomainName', '//example.com']); _gaq.push(['_trackPageview']);
@@ -32,11 +32,30 @@
 				<h1 class="title">Hello friend.</h1>
 				<p>One Black Crayon is a solo front-end web development, and email marketing & custom HTML email development shop. With experience in building custom static and WordPress based websites, One Black Crayon can help you with your projects, join your team in building your site, help you with that pesky email template, and even help you with that newly built website or web application that your expensive web development company just handed over to you.</p>
 				<p>Over the past 15 years, I have helped clients big and small get the results they needed for the businesses. From tasks that you wouldn't believe you could outsource to working with design &amp; development firms; I put your needs first.</p>
-				<p>Since I am currently available for hire/consultations, <a href="">we should talk</a> about how <b>One Black Crayon</b> can help you with projects.</p>
+				<p>Since I am currently available for hire/consultations, <a id="example2" href="#modal">we should talk</a> about how <b>One Black Crayon</b> can help you with projects.</p>
 			</div>
 			<footer role="contentinfo">
 				<h1 class="copy">&copy; Copyright <?php echo date('Y');?>. Frederick Polk. All Rights Reserved.</span></h1>
 			</footer>
 		</article>
+		<div id="modal" style="display: none;" class="modal-example-content">
+			<div class="close" onclick="$.fn.custombox('close');">&times;</div>
+			<iframe class="iframe" style="width:640px;height:644px;border:none;" allowTransparency="true" frameborder="0" scrolling="no" src="https://frederickpolk.wufoo.com/embed/z110k1cv0p0wkuu/"></iframe><div id="wuf-adv" style="font-family:inherit;font-size: small;color:#a7a7a7;text-align:center;display:block;">HTML Forms powered by <a href="http://www.wufoo.com">Wufoo</a></div>
+		</div>
+		<script src="assets/jquery-1.10.2.js"></script>
+		<script src="assets/jquery.custombox.js"></script>
+		<script>
+		$(function () {
+			$('#example2').on('click', function ( e ) {
+				$.fn.custombox( this, {
+					responsive: true,
+					effect: 'fadein',
+					overlayColor:   '#110111',
+					overlayOpacity: 1
+				});
+				e.preventDefault();
+			});
+		});
+		</script>
 	</body>
 </html>
