424fe5254adc90c9693c0adad26b51929af32df3
Frederick Polk <github@oneblackcrayon.com>
2014-02-11 18:42:13 -0500

    Added Google Analytics.

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		<script src="assets/modernizr.js"></script>
 		<!-- make sure your google analytics and other analytics are up to date with correct code -->
 		<script type="text/javascript">
-		(function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-XXXXX-X']); _gaq.push(['_setDomainName', '//example.com']); _gaq.push(['_trackPageview']);
+		(function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-35323954-1']); _gaq.push(['_setDomainName', '//oneblackcrayon.com']); _gaq.push(['_trackPageview']);
 		</script>
 		<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
