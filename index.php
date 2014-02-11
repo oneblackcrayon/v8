d409c6fa1e038056d4f7b81d69e7201f18ba6ad1
Frederick Polk <github@oneblackcrayon.com>
2014-02-11 14:17:59 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en-us">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="robots" content="index,nofollow">
+		<meta name="author" content="Frederick Polk">
+		<meta name="description" content="One Black Crayon is a solo web studio that makes websites, crafts HTML emails, and helps with internet presences.">
+		<link rel="stylesheet" href="style.css">
+		<!-- add more css here -->
+		<!-- add more javascript here -->
+		<!-- make sure your google analytics and other analytics are up to date with correct code -->
+		<script type="text/javascript">
+		(function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-XXXXX-X']); _gaq.push(['_setDomainName', '//example.com']); _gaq.push(['_trackPageview']);
+		</script>
+		<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+		<meta name="msapplication-TileColor" content="#D83434">
+		<meta name="msapplication-TileImage" content="/tileicon.png">
+		<title>&nbsp;</title>
+	</head>
+	<body role="document" aria-labelledby="document-desc">
+		<article role="main">
+			<header role="banner">
+				<div class="logo">
+					<img src="logo.svg" alt="Logo">
+				</div>
+				<h1 class="intro">One Black Crayon</h1>
+				<p class="summary">Makes websites, crafts HTML email, and helps with internet presences.</p>
+			</header>
+			<div class="entry">
+				<h1 class="title">Hello friend.</h1>
+				<p>One Black Crayon is a solo front-end web development, and email marketing & custom HTML email development shop. With experience in building custom static and WordPress based websites, One Black Crayon can help you with your projects, join your team in building your site, help you with that pesky email template, and even help you with that newly built website or web application that your expensive web development company just handed over to you.</p>
+				<p>Over the past 15 years, I have helped clients big and small get the results they needed for the businesses. From tasks that you wouldn't believe you could outsource to working with design &amp; development firms; I put your needs first.</p>
+				<p>Since I am currently available for hire/consultations, <a href="">we should talk</a> about how <b>One Black Crayon</b> can help you with projects.</p>
+			</div>
+			<footer role="contentinfo">
+				<h1 class="copy">&copy; Copyright <?php echo date('Y');?>. Frederick Polk. All Rights Reserved.</span></h1>
+			</footer>
+		</article>
+	</body>
+</html>
